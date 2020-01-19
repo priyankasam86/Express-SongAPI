b7072ab8ebea08796907e81885d9d90ded477eb7
priyankasam86 <priyanka_dalvi86@yahoo.co.in>
2020-01-20 01:44:02 +0530

    Final Commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let express = require("express");  //Express is a in-built module
 let app = express(); // we have invoked 'express' module here and assigned it to the 'app' variable
 //console.log(app);
 let port = process.env.NODE_ENV || 4800;
+app.use(express.json()); // in-built middleware to accept JSON data in express js
+let Joi = require("@hapi/joi");
 
 let songs = [
     {id: 1, name:"AAAAAA", singer: "Ashwini",  duration: "4 min",   price:1000},
@@ -14,7 +16,7 @@ let songs = [
 
 // -----Fetch All Song Data
 app.get("/api/allsongs", (req,res) => {
-    //res.send("Hello");
+    //res.send("Hello");    
     res.send(songs);
 });
 
@@ -26,8 +28,68 @@ app.get("/api/song/:id", (req, res) => {
     if(!song){ return res.status(404).send({message : "Invalid song ID"}) };
     //res.send(song);
     let {id, name, singer, duration, price} = song;
-    res.send(singer);
-})
+    res.send(name);
+});
 
+//------Create a new Song data
+app.post("/api/song/newsong", ( req, res ) =>{
+    let {error} = validationError(req.body);
+    if(error){ return res.send(error.details[0].message)}
+
+    let song = {
+        id : songs.length + 1,
+        name : req.body.name, 
+        singer : req.body.singer,
+        duration : req.body.duration,
+        price : req.body.price
+    }
+    songs.push(song);
+    res.send(songs);
+
+});
+
+//------Update song-------
+
+app.put("/api/song/updatesong/:id", (req, res) => { 
+
+    // ID Check
+    let song = songs.find(item => item.id === parseInt(req.params.id));
+    if(!song){ return res.status(404).send({message : "Invalid song ID"}) };
+
+    //Validate using Joi 
+    let {error} = validationError(req.body);
+    if(error){ return res.send(error.details[0].message)}
+
+    //Save
+    song.name = req.body.name;
+    song.singer = req.body.singer;
+    song.duration = req.body.duration;
+    song.price = req.body.price;
+    res.send(songs);
+});
+
+//------Remove Song------
+app.delete("/api/song/removesong/:id", (req,res) => {
+
+    //ID Check
+    let song = songs.find(item => item.id === parseInt(req.params.id));
+    if(!song){ return res.status(404).send({message : "Invalid song ID"}) };
+
+    let index = songs.indexOf(song);
+    songs.splice(index,1);
+    res.sen({message: "Removed the selected Song", s: songs});
+
+});
+
+function validationError(error)
+{
+    let Schema = Joi.object({
+        name : Joi.string().min(3).required(),
+        singer : Joi.string().min(3).required(),
+        duration : Joi.string().alphanum().required(),
+        price : Joi.number().required()
+    });
+    return Schema.validate(error);
+}
 
 app.listen(port, () => {console.log(`port working on ${port}`)});
